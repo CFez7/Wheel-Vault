c4c12c9ce1e4a4926495f5e6b2f978ee3f572bc5
Connor Ferry <i7629817@bournemouth.ac.uk>
2017-09-13 16:36:05 +0100

    Comment updates to changeAccount.php
    Comment updates to changeAccount.php

--- public_html/scripts/changeAccount.php
+++ public_html/scripts/changeAccount.php
@@ -1,9 +1,10 @@
 <?php
+    // link database and session
     require_once("../../includes/session.php");
     require_once("../../includes/connect.php");
 ?>
 <?php 
-        
+      // If change details has been pressed, check data base be passed through or error.
       if(isset($_POST["changeDetails"])) {
         $name = $_POST["changename"];
         $email = $_POST["changeemail"];
@@ -17,6 +18,7 @@
     }
 ?>
 <?php
+        // If any fields are blank, prompt user.
         if(empty($_POST['changeemail'])) {
             $_SESSION['accountmessage'] = "Email field cannot be left empty!";
             header("Location: ../account.php");
@@ -27,12 +29,15 @@
         } else { 
 ?>
 <?php
+       // If password entered matches that in the database (encrypted), go ahead with the update.
        if(crypt($upassword, $_SESSION["upassword"]) == $_SESSION["upassword"]) { 
            
+           // Set query update to replace all details where userID matches that of the user logged in.
             $query = "UPDATE users SET name='{$name}', email='{$email}', phone='{$phone}', location='{$location}' WHERE id='{$userID}'";
 
             $result = mysqli_query($connection, $query); 
 
+            // Change all session details to the updated versions
             $_SESSION["userID"] = $userID;
             $_SESSION["email"] = $email;
             $_SESSION["name"] = $name;
@@ -51,6 +56,7 @@
 ?>
 <?php 
     if(isset($_POST["addpost"])) {
+        // Close database connection
         mysqli_close($connection);
     }
 ?>
