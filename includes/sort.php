21f2ff6947e6d8f7bacb1603abb0a3d8b32a0ed0
Connor Ferry <i7629817@bournemouth.ac.uk>
2017-09-13 16:49:11 +0100

    DISTINCT Drop downs with comments
    Added DISTINCT queries to only show drop down items that are in database.

--- includes/sort.php
+++ includes/sort.php
@@ -1,46 +1,85 @@
 <!-- the actual filter form made up of drop down lists and a submit button. Submits to this page -->
 <div class="sortbar">
     <form action="index.php" method="post" style="display:inline;">
-        <select name="size">
-            <option value="">Wheel Size</option>
-            <option value="15">15"</option>
-            <option value="16">16"</option>
-            <option value="17">17"</option>
-            <option value="18">18"</option>
-            <option value="19">19"</option>
+        <select class="sortInput" name="size">
+            <option value=""> Size </option>
+            <?php
+            
+                // Populate dropdown lists ONLY with options that are in the database already to reduce blank search results
+            
+                $query = "SELECT DISTINCT size FROM listings ORDER BY size ASC";
+                $result = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($result)) {
+             ?>
+            <option value="<?php echo $row['size']; ?>">
+                      <?php echo ucfirst($row['size']); ?>
+             </option>
+             <?php
+                }
+              ?>
         </select>
-        <select name="brand">
-            <option value="">Brand</option>
-            <option value="bbs">BBS</option>
-            <option value="3sdm">3SDM</option>
-            <option value="oem">OEM</option>
-            <option value="oem">BMW</option>
-            <option value="rotiform">Rotiform</option>
+        <select class="sortInput" name="brand">
+            <option value=""> Brand </option>
+            <?php
+                $query = "SELECT DISTINCT brand FROM listings ORDER BY brand ASC";
+                $result = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($result)) {
+             ?>
+            <option value="<?php echo $row['brand']; ?>">
+                      <?php echo ucfirst($row['brand']); ?>
+             </option>
+             <?php
+                }
+              ?>
         </select>
-        <select name="width">
-            <option value="">Width</option>
-            <option value="6.5">6.5j</option>
-            <option value="7">7j</option>
-            <option value="7.5">7.5j</option>
-            <option value="8">8j</option>
-            <option value="8.5">8.5j</option>
-            <option value="9">9j</option>
-            <option value="9.5">9.5j</option>
+        <select class="sortInput" name="width">
+            <option value=""> Width </option>
+            <?php
+                $query = "SELECT DISTINCT frontwidth, rearwidth FROM listings ORDER BY frontwidth, rearwidth ASC";
+                $result = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($result)) {
+             ?>
+            <option value="<?php echo $row['frontwidth']; ?>">
+                      <?php echo ucfirst($row['frontwidth']); ?>
+             </option>
+            <option value="<?php echo $row['rearwidth']; ?>">
+                      <?php echo ucfirst($row['rearwidth']); ?>
+             </option>
+             <?php
+                }
+              ?>
         </select>
-        <select name="offset">
-            <option value="">Offset</option>
-            <option value="25">25</option>
-            <option value="35">35</option>
-            <option value="45">45</option>
-            <option value="55">55</option>
+        <select class="sortInput" name="offset">
+            <option value=""> Offset </option>
+            <?php
+                $query = "SELECT DISTINCT frontoffset, rearoffset FROM listings ORDER BY frontoffset, rearoffset ASC";
+                $result = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($result)) {
+             ?>
+            <option value="<?php echo $row['frontoffset']; ?>">
+                      <?php echo ucfirst($row['frontoffset']); ?>
+             </option>
+            <option value="<?php echo $row['rearoffset']; ?>">
+                      <?php echo ucfirst($row['rearoffset']); ?>
+             </option>
+             <?php
+                }
+              ?>
         </select>
-        <select name="studpattern">
-            <option value="">Stud Pattern</option>
-            <option value="4x100">4x100</option>
-            <option value="5x100">5x100</option>
-            <option value="5x110">5x110</option>
-            <option value="5x112">5x112</option>
+        <select class="sortInput" name="studpattern">
+            <option value=""> Stud Pattern </option>
+            <?php
+                $query = "SELECT DISTINCT studpattern FROM listings ORDER BY studpattern ASC";
+                $result = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_assoc($result)) {
+             ?>
+            <option value="<?php echo $row['studpattern']; ?>">
+                      <?php echo ucfirst($row['studpattern']); ?>
+             </option>
+             <?php
+                }
+              ?>
         </select>
-        <input type="submit" name="filter" value="Filter Results" style="float:right;" />
+        <input class="sortInput" id="sortSubmit" type="submit" name="filter" value="Filter Results" style="float:right;" />
     </form>
 </div>
